1551587975aaee37188477ade7676d17cacfc738
limited.eternity <nabin.kaucha@gmail.com>
2022-04-01 20:10:15 +0545

    fairly intelligent computer counter play

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//array of wind conditions
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,9 +11,11 @@ const winConditions = [
 ];
 
 const gameboard = (() => {
+    //initialize board as array with all 0's
     let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     const getBoard = () => board;
 
+    //return unmarked boards for ai
     const getUnmarkedSquares = () => {
         let unmarkedSquares = [];
         board.forEach((value, index) => {
@@ -23,10 +26,12 @@ const gameboard = (() => {
         return unmarkedSquares;
     }
 
+    //update board array according to player marking
     const updateBoard = (sign, index) => {
         board[index] = sign;
     }
 
+    //reset board to initial state
     const resetBoard = () => {
         board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
@@ -47,6 +52,7 @@ const display = (() =>{
     const turnField = document.getElementsByClassName('showTurn')[0];
     const winnerField = document.getElementsByClassName('showWinner')[0];
 
+    //display game board according to current state of main board array
     const renderBoard = () => {
         gameBoardEl.innerHTML = "";
         gameboard.getBoard().forEach(value => {
@@ -61,6 +67,7 @@ const display = (() =>{
         })
     };
 
+    //get types for both player from radio selection, if nothing selected, raise error
     const getPlayerTypes = () => {
         playerOneErrorField.textContent = "";
         playerTwoErrorField.textContent = "";
@@ -101,6 +108,7 @@ const display = (() =>{
         return true;
     }
 
+    //function to display who's turn it is to play
     const showWhosTurn = (player) => {
         if(player.name){
             turnField.textContent = `${player.name}'s turn`;
@@ -110,6 +118,7 @@ const display = (() =>{
         }
     }
 
+    //function to display if player won game or it's a tie
     const showWinCondition = (player) => {
         if(player){
             winnerField.textContent = `${player.name} won`;
@@ -117,18 +126,21 @@ const display = (() =>{
         else {
             winnerField.textContent = "It's a tie";
         }
+        turnField.textContent = '';
     }
 
+    //clear turn information and winner information
     const clearAll = () => {
         turnField.textContent = '';
         winnerField.textContent = '';
     }
 
-    return {startButton, gameContainer, gameBoardEl, renderBoard, getPlayerTypes, showWhosTurn, showWinCondition, clearAll};
+    return {playerOneRadio, playerOneNameInput, playerTwoRadio, playerTwoNameInput, startButton, gameContainer, gameBoardEl,
+            renderBoard, getPlayerTypes, showWhosTurn, showWinCondition, clearAll};
 })();
 
 const game = (() => {
-
+    //initalize two players
     let playerOne = {
         name: '',
         sign: 'X',
@@ -141,18 +153,22 @@ const game = (() => {
         type: '',
     };
 
-    let turn = playerTwo;
+    //player one goes first
+    let turn = playerOne;
 
+    //function to initialize game by making 'start game' button loaded with click function
     const init = () => {
         display.startButton.addEventListener('click', initEventFunctionWrapper);
     };
 
+    //wrapper function for 'start game' button so we can remove the event listener later
     const initEventFunctionWrapper = () => {
         if(display.getPlayerTypes()){
             startGame();
         }
     }
 
+    //wrapper function for 'reset game' button so we can remove event listener later
     const resetEventFunctionWrapper = () => {
         display.startButton.value = 'Start game';
         display.startButton.removeEventListener('click', resetEventFunctionWrapper);
@@ -161,6 +177,7 @@ const game = (() => {
         display.clearAll();
     }
 
+    //if all player type is selected, initialize gameboard and start playing
     const startGame = () => {
         display.startButton.value = 'Reset game';
         display.startButton.removeEventListener('click', initEventFunctionWrapper);
@@ -168,24 +185,31 @@ const game = (() => {
         gameboard.resetBoard();
         display.gameContainer.style.display = 'flex';
         display.renderBoard();
+        turn = playerOne;
         playGame();
     };
 
+    //play game turn by turn
     const playGame = () => {
         if(!checkWin()){
-            changePlayerTurn();
             display.showWhosTurn(turn);
+            firstPlay = false;
             if(turn.type === 'human'){
                 humanPlay(turn.sign);
             }
             else if(turn.type === 'computer'){
                 setTimeout(() => {
                     computerPlay(turn.sign)
-                }, 1500);
+                }, 1500);                       //delay computer play by 1.5 seconds
             }
+            return;
         }
-        if(checkWin()){
+        if(checkWin() === true){
+            changePlayerTurn();
             display.showWinCondition(turn);
+        }
+        else if(checkWin() === 'tie'){
+            display.showWinCondition('');
         }
     }
 
@@ -198,6 +222,7 @@ const game = (() => {
         }
     }
 
+    //function for human player
     const humanPlay = (sign) => {
         let squares = Array.from(display.gameBoardEl.getElementsByClassName('square'));
         squares.forEach(squre => {
@@ -206,20 +231,80 @@ const game = (() => {
                     let indexOfSquare = squares.indexOf(e.target);
                     gameboard.updateBoard(sign, indexOfSquare);
                     display.renderBoard();
+                    changePlayerTurn();
                     playGame();
                 }
             }, {once: true});
         })
     }
 
+    //logic for computer to (counter)play
     const computerPlay = (sign) => {
         const options = gameboard.getUnmarkedSquares();
-        let a = Math.floor(Math.random() * options.length);
-        gameboard.updateBoard(sign, options[a]);
+        const currentBoard = gameboard.getBoard();
+        let mine = [], opponent = [];
+        let counter = 0;
+        currentBoard.forEach((value, index) => {
+            if(value != sign && value != 0){
+                opponent.push(index);
+            }
+            else if(value === sign && value != 0){
+                mine.push(index);
+            }
+        });
+
+        let indexToMark = -1;
+
+        winConditions.forEach(condition => {
+            condition.forEach(value => {
+                if(opponent.includes(value)){
+                    counter++;
+                }
+            })
+            if(counter === 2){
+                condition.forEach(value => {
+                    if(!opponent.includes(value) && options.includes(value) && (indexToMark < 0)){
+                        indexToMark = value;
+                    }
+                })
+                counter = 0;
+            }
+            else{
+                counter = 0;
+            }
+        })
+
+        if(indexToMark < 0){
+            winConditions.forEach(condition => {
+                condition.forEach(value => {
+                    if(mine.includes(value)){
+                        counter++;
+                    }
+                })
+                if(counter === 2){
+                    condition.forEach(value => {
+                        if(!mine.includes(value) && options.includes(value) && (indexToMark < 0)){
+                            indexToMark = value;
+                        }
+                    })
+                    counter = 0;
+                }
+                else{
+                    counter = 0;
+                }
+            })
+        }
+
+        if(indexToMark < 0){
+            indexToMark = options[Math.floor(Math.random() * options.length)];
+        }
+        gameboard.updateBoard(sign, indexToMark);
         display.renderBoard();
+        changePlayerTurn();
         playGame();
     }
 
+    //compare current game board array against win conditions to check win condition
     const checkWin = () => {
         let currentBoard = gameboard.getBoard();
         let xCounter, oCounter;
@@ -248,11 +333,13 @@ const game = (() => {
             return true;
         }
         if(!won && (currentBoard.indexOf(0) == -1)){
-            return true;
+
+            return 'tie';
         }
         return false;
     }
 
+    //start
     init();
 
     return {playerOne, playerTwo, checkWin};
